419a9ddc91ca4888d63fc8053d9b8ee087129f17
GhostFire90 <31078396+GhostFire90@users.noreply.github.com>
2020-12-30 15:37:24 -0800

    Init

---
+++ SHVRGodmode.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using MelonLoader;
+using UnityEngine;
+[assembly : MelonInfo(typeof(SHVRGodMode.SHVRGodmode), "Superhot VR Godmode", "1.0", "Ghostfire04")]
+[assembly : MelonGame("SUPERHOT_Team", "SUPERHOT_VR")]
+namespace SHVRGodMode
+{
+    class SHVRGodmode : MelonMod
+    {
+        static bool im;
+        static GameObject player;
+        public override void OnLevelWasLoaded(int level)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.F3))
+            {
+                DebugSettings.Instance.devMenu = true;
+                VrDevMenu.Init();
+            }
+        }
+        [HarmonyPatch(typeof(VrDevMenu),"Awake", MethodType.Normal)]
+        private static class VrDevMenu_Patch
+        {
+            private static void Prefix(VrDevMenu __instance)
+            {
+                DebugSettings.Instance.devMenu = true;
+            }
+            private static void Postfix(VrDevMenu __instance)
+            {
+                VrDevMenu.RemoveAction("godmode");
+                VrDevMenu.AddAction("Ghostfire042's Godmode : " + im, delegate
+                {
+                    im = !im;
+                    VrDevMenu.RelabelAction("firegodmode", "Ghostfire042's Godmode : " + im);
+                }, false, "firegodmode");
+            }
+        }
+        [HarmonyPatch(typeof(PlayerActionsVR), "OnBulletHit", MethodType.Normal)]
+        private static class AntiBullet
+        {
+            private static bool Prefix()
+            {
+                if (im)
+                {
+                    return false;
+                } 
+                else
+                {
+                    return true;
+                }
+            }
+        }
+        [HarmonyPatch(typeof(PlayerActionsVR), "Kill", MethodType.Normal)]
+        private static class AntiMelee
+        {
+            private static bool Prefix()
+            {
+                if (im)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+    }
+}
